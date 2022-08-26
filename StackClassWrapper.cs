d8350684ec2087e3563cd021b81b6ba2a8d3e57d
Dave Bage <david_bage@hotmail.com>
2022-08-26 17:35:10 +0100

    Add project files.

---
+++ StackClassWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Dynamic;
+
+namespace WarmUpStackClass
+{
+    public class StackClassWrapper
+    {
+        private Stack myStack;
+
+        public StackClassWrapper()
+        {
+            myStack = new Stack();
+        }
+
+        public void Push(object @object)
+        {
+            myStack.Push(@object);
+        }
+
+        public Object Pop()
+        {
+            return myStack.Pop();
+        }
+
+        public Object Peek()
+        {
+            return myStack.Peek();
+        }
+
+
+    }
+}
